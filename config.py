03e5a9bba0247cf5ee313989c157710b60ad7c2f
sagorsakh14-lab <sagorsakh14@gmail.com>
2026-04-08 01:10:59 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py — TachZone Hosting Bot
+
+BOT_TOKEN  = "8734287278:AAE3gUtc5-sF1MD4FnVKlqnXWamtmfXkJOY"
+ADMIN_ID   = 7318114944
+
+BASE_DIR   = "users"
+DB_FILE    = "tachzone.db"
+MAX_BOTS   = 5
