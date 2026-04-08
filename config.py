c6543e8370e17e188a3eaeb5c525be3ca1e03a90
sagorsakh14-lab <sagorsakh14@gmail.com>
2026-04-08 10:21:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,8 @@
 # config.py — TachZone Hosting Bot
 
-import os
+BOT_TOKEN  = "8369483375:AAE3pRU0Pc1XDbDImiH0uKfV_cQmVDp2c"
+ADMIN_ID   = 122532171
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8734287278:AAE3gUtc5-sF1MD4FnVKlqnXWamtmfXkJOY")
-ADMIN_ID  = int(os.environ.get("ADMIN_ID", "7318114944"))
-MAX_BOTS  = int(os.environ.get("MAX_BOTS", "5"))
-
-# Railway Volume → /data এ persistent storage
-_DATA_DIR = "/data"
-os.makedirs(_DATA_DIR, exist_ok=True)
-
-DB_FILE  = os.path.join(_DATA_DIR, "tachzone.db")
-BASE_DIR = os.path.join(_DATA_DIR, "users")
-os.makedirs(BASE_DIR, exist_ok=True)
+BASE_DIR   = "users"
+DB_FILE    = "tachzone.db"
+MAX_BOTS   = 5
