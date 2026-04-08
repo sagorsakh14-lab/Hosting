342d64abe458be086149f01ed45965eb51f80808
sagorsakh14-lab <sagorsakh14@gmail.com>
2026-04-08 10:25:29 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py — TachZone Hosting Bot
 
-BOT_TOKEN  = "8369483375:AAE3pRU0Pc1XDbDImiH0uKfV_cQmVDp2c"
-ADMIN_ID   = 122532171
+BOT_TOKEN  = "8734287278:AAE3gUtc5-sF1MD4FnVKlqnXWamtmfXkJOY"
+ADMIN_ID   = 7318114944
 
 BASE_DIR   = "users"
 DB_FILE    = "tachzone.db"
